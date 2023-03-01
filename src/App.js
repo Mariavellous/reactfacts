4386cc1b1d7ab339c80776acfc2af1e3f47e9fba
Mariavellous <mm.alcaide38@gmail.com>
2023-02-28 21:13:46 -0800

    add Main file to App

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react"
 import Navbar from "./components/Navbar"
+import Main from "./components/Main"
 import './App.css';
 
 
@@ -8,6 +9,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <Navbar />
+        <Main />
         <p>
           Hello Melanie Alzugray!
         </p>
